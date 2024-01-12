76e6ca751b3bb640ddf73ae8b88bbbe44d891ea1
JaeHeon Lee <heonme@icloud.com>
2024-01-12 12:16:25 +0900

    Publish

--- Program.cs
+++ Program.cs
@@ -51,7 +51,7 @@ namespace AreaTM_acbas
         private static string appGUID = "areatmgekimoestreamassistant";
 
         public static string acbas_ver = "5.16_A";
-        public static long acbas_build = 202401121139;
+        public static long acbas_build = 202401121216;
         public static string acbas_partnum = "v5_5";
 
         //public static string ad_servercountry = "KR";
