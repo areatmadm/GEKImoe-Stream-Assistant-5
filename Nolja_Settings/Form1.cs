e77040496ae2b1fb8b683c737908560ed76aa9f4
JaeHeon Lee <heonme@icloud.com>
2024-03-29 15:53:33 +0900

    GEKImoe Stream Assistant Fix does not execute reboot for bypass capture system

--- Nolja_Settings/Form1.cs
+++ Nolja_Settings/Form1.cs
@@ -16,7 +16,7 @@ namespace GEKImoeStreamAssistant5FixReboot
 {
     public partial class Nolja_ErrorFix : Form
     {
-        int quick = 140;
+        int quick = 30;
         int qtemp;
 
         private string GetHtmlString(string url)
@@ -37,6 +37,50 @@ namespace GEKImoeStreamAssistant5FixReboot
             catch
             {
                 return "Error";
+            }
+        }
+
+        public static string PostHtmlString(string url, String[] postDataKey, String[] postDataValue) //POST 전송에 필요한 데이터 수집
+        {
+            try
+            {
+
+                String callUrl = url;
+                //String[] data = new String[1];
+
+                String postDataToSend = null;
+                for (int i = 0; i < postDataKey.Length; i++) //값 전달할 key 전달
+                {
+                    if (i > 0) postDataToSend += "&";
+                    postDataToSend += postDataKey[i];
+                    postDataToSend += "=";
+                    postDataToSend += postDataValue[i];
+                }
+
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(callUrl);
+
+                //인코딩 UTF-8
+                byte[] sendData = UTF8Encoding.UTF8.GetBytes(postDataToSend);
+                httpWebRequest.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+                httpWebRequest.Method = "POST";
+                httpWebRequest.ContentLength = sendData.Length;
+
+                Stream requestStream = httpWebRequest.GetRequestStream();
+                requestStream.Write(sendData, 0, sendData.Length);
+                requestStream.Close();
+
+                HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream(), Encoding.GetEncoding("UTF-8"));
+                String response = streamReader.ReadToEnd();
+
+                streamReader.Close();
+                httpWebResponse.Close();
+
+                return response;
+            }
+            catch
+            {
+                return "__Error__";
             }
         }
 
@@ -64,8 +108,8 @@ namespace GEKImoeStreamAssistant5FixReboot
         {
             PrivateFontCollection font_3_0_s = new PrivateFontCollection();
             string fix_nfd;
-            fix_nfd = System.IO.Path.GetFullPath("font_3.0").Replace(@"Bugfix\", "");
-            //fix_nfd = System.IO.Path.GetFullPath("font_3.0").Replace(@"Debug\", "");
+            //fix_nfd = System.IO.Path.GetFullPath("font_3.0").Replace(@"Bugfix\", "");
+            fix_nfd = System.IO.Path.GetFullPath("font_3.0").Replace(@"Debug\", "");
             //MessageBox.Show(fix_nfd);
             font_3_0_s.AddFontFile(fix_nfd + @"\nanum-barun-gothic\NanumBarunGothicBold.otf");
             font_3_0_s.AddFontFile(fix_nfd + @"\nanum-barun-gothic\NanumBarunGothic.otf");
@@ -85,22 +129,25 @@ namespace GEKImoeStreamAssistant5FixReboot
             btn_fix.Enabled = false;
             btn_exit.Enabled = false;
 
-            Process.Start("nolja_fix_pp.vbs"); //SW 강종 코드
+            //Process.Start("nolja_fix_pp.vbs"); //SW 강종 코드
             Delay(6000);
 
             string noll;
             string setgame = "";
             string vender = "";
-            if (File.Exists(Path.GetFullPath("nolja_game_set.txt").Replace(@"Bugfix\", ""))) //놀자 독자규격 사용 시
+            /*if (File.Exists(Path.GetFullPath("nolja_game_set.txt").Replace(@"Bugfix\", ""))) //놀자 독자규격 사용 시
             {
                 setgame = File.ReadAllText(Path.GetFullPath("nolja_game_set.txt").Replace(@"Bugfix\", ""));
             }
             else //표준규격 사용 업장
             {
                 setgame = File.ReadAllText(Path.GetFullPath("game_set.txt").Replace(@"Bugfix\", ""));
-            }
+            }*/
+            //표준규격 단일화로 인한 코드 제거 진행 중(Ver.5.17부터 적용)
 
-            if(File.Exists(Path.GetFullPath("vender.txt").Replace(@"Bugfix\", ""))) vender = File.ReadAllText(Path.GetFullPath("vender.txt").Replace(@"Bugfix\", ""));
+            setgame = File.ReadAllText(Path.GetFullPath("game_set.txt").Replace(@"Bugfix\", ""));
+
+            if (File.Exists(Path.GetFullPath("vender.txt").Replace(@"Bugfix\", ""))) vender = File.ReadAllText(Path.GetFullPath("vender.txt").Replace(@"Bugfix\", ""));
             else vender = "NOLJA";
 
             if (File.Exists(Path.GetFullPath("nolja_game_set.txt").Replace(@"Bugfix\", ""))) //놀자는 독자적인 도메인 이용
@@ -114,9 +161,9 @@ namespace GEKImoeStreamAssistant5FixReboot
             
             Delay(1000);
 
-            lbl_name.Text = "스트리밍PC 재부팅 요청됨";
+            lbl_name.Text = "GEKImoe Stream Assistant 재시작 요청됨";
             timer1.Enabled = true;
-            label1.Text = "1분 10초 후 재부팅을 시작합니다.";
+            label1.Text = "30초 후 스트리밍을 재시작합니다.";
         }
 
         private void btn_exit_Click(object sender, EventArgs e)
@@ -131,16 +178,16 @@ namespace GEKImoeStreamAssistant5FixReboot
 
             if (qtemp >= 60)
             {
-                label1.Text = (qtemp / 60) + "분 " + (qtemp % 60) + "초 후 재부팅을 시작합니다.";
+                label1.Text = (qtemp / 60) + "분 " + (qtemp % 60) + "초 후 재시작합니다.";
             }
-            else label1.Text = qtemp + "초 후 재부팅을 시작합니다.";
+            else label1.Text = qtemp + "초 후 재시작합니다.";
 
             if (qtemp < 0)
             {
                 label1.Text = "재부팅을 진행합니다...";
                 timer1.Enabled = false;
                 Delay(2000);
-                Process.Start(@"nolja_reboot.vbs");
+                //Process.Start(@"nolja_reboot.vbs");
             }
 
             //label1.Text = "3분 30초 후 재부팅을 시작합니다.";
