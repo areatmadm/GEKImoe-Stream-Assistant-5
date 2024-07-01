11b73ca484471e3adafb21fd037d1a507f826c9f
JaeHeon Lee <heonme@icloud.com>
2024-07-01 19:28:58 +0900

    Revert "TestVersionBuild"
    This reverts commit b85ef90d437e79dc042d60ccd7a360feef95e226.

--- Program.cs
+++ Program.cs
@@ -136,7 +136,7 @@ namespace AreaTM_acbas
 
         private static string appGUID = "areatmgekimoestreamassistant";
 
-        public static string acbas_ver = "5.20_A_TS";
+        public static string acbas_ver = "5.20_A";
         public static long acbas_build = 202406171637;
         public static string acbas_partnum = "v5_5";
 
