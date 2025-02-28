451c467f77754504dd6d9af0559ddb3622ad9e1f
JaeHeon Lee <heonme@icloud.com>
2025-02-28 22:40:12 +0900

    restream delay time changed(55sec → 60sec)

--- Program.cs
+++ Program.cs
@@ -137,8 +137,8 @@ namespace AreaTM_acbas
 
         private static string appGUID = "areatmgekimoestreamassistant";
 
-        public static string acbas_ver = "5.23_B";
-        public static long acbas_build = 202502261648;
+        public static string acbas_ver = "5.23_C";
+        public static long acbas_build = 202502282239;
         public static string acbas_partnum = "v5_7";
 
         //public static string ad_servercountry = "KR";
